8003c7fa1aca490ecdee9a1a92d6ca2988f38ae0
manrick <manricksantos@gmail.com>
2023-11-19 12:26:05 -0300

    cria rota das páginas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-export default function App(): JSX.Element {
-  return (
-    <>
-      <h1>app</h1>
-    </>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import New from "./pages/dashboard/new";
+import Dashboard from "./pages/dashboard";
+import DetailCar from "./pages/detailCar";
+
+import Layout from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/car/:id",
+        element: <DetailCar />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export { router };
